5049d3b3d7085f0b4e2007d2433d1065d90683d8
Maksimka2280 <maksimkalol1402@gmail.com>
2026-08-02 16:00:50 +0300

    fix(api) fix api for versel

--- service/instance.ts
+++ service/instance.ts
@@ -3,3 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
 baseURL: process.env.NEXT_PUBLIC_API_URL, // NEXT_PUBLIC - обовязковий 
 })
+
+
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
